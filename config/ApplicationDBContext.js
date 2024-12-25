c32b09ef27f148522083c0e35e800ae6b67610bc
Kakashi-Pro <avinash74vk@gmail.com>
2024-12-25 15:00:56 +0530

    fixed2

--- config/ApplicationDBContext.js
+++ config/ApplicationDBContext.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDb = async () => {
   try {
-    await mongoose.connect('mongodb+srv://Avinash:bHMdG1VZAehKwu7j@temp-pro.qnun8.mongodb.net/?retryWrites=true&w=majority&appName=Temp-pro', {
+    await mongoose.connect('mongodb+srv://Avinash:bHMdG1VZAehKwu7j@temp-pro.qnun8.mongodb.net/test=true&w=majority&appName=Temp-pro', {
     });
     console.log("DB is connected successfully.");
   } catch (err) {
